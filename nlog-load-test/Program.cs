ed4b3e285353cc6a210ec0ae775e6aaa7558d603
Dmitry Novik <dmitry.novik@gmail.com>
2018-09-10 16:45:31 +1000

    Add project files.

---
+++ nlog-load-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nlog_load_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
